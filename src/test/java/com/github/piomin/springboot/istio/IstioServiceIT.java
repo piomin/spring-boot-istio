f49ab26085af6f68af2aa70c588b4a697490e360
piomin <piotr.minkowski@gmail.com>
2026-01-04 12:08:27 +0100

    #162 Fix test default namespace

--- src/test/java/com/github/piomin/springboot/istio/IstioServiceIT.java
+++ src/test/java/com/github/piomin/springboot/istio/IstioServiceIT.java
@@ -2,6 +2,8 @@ package com.github.piomin.springboot.istio;
 
 import io.fabric8.istio.api.networking.v1beta1.Gateway;
 import io.fabric8.istio.api.networking.v1beta1.VirtualService;
+import io.fabric8.istio.client.DefaultIstioClient;
+import io.fabric8.istio.client.IstioClient;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
@@ -10,6 +12,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
 
@@ -22,17 +26,22 @@ public class IstioServiceIT {
     @BeforeAll
     static void setup(@Value("${k8s.context}") String k8sContext) throws IOException, InterruptedException {
         Config config = Config.autoConfigure(k8sContext);
+        config.setNamespace("default");
+        config.setDefaultNamespace(true);
         try (KubernetesClient client = new KubernetesClientBuilder().withConfig(config).build()) {
             System.setProperty(Config.KUBERNETES_MASTER_SYSTEM_PROPERTY, client.getConfiguration().getMasterUrl());
-            System.setProperty(Config.KUBERNETES_CLIENT_CERTIFICATE_FILE_SYSTEM_PROPERTY,
+            if (client.getConfiguration().getClientCertFile() != null)
+                System.setProperty(Config.KUBERNETES_CLIENT_CERTIFICATE_FILE_SYSTEM_PROPERTY,
                     client.getConfiguration().getClientCertFile());
-            System.setProperty(Config.KUBERNETES_CA_CERTIFICATE_FILE_SYSTEM_PROPERTY,
+            if (client.getConfiguration().getCaCertFile() != null)
+                System.setProperty(Config.KUBERNETES_CA_CERTIFICATE_FILE_SYSTEM_PROPERTY,
                     client.getConfiguration().getCaCertFile());
-            System.setProperty(Config.KUBERNETES_CLIENT_KEY_FILE_SYSTEM_PROPERTY,
+            if (client.getConfiguration().getClientKeyFile() != null)
+                System.setProperty(Config.KUBERNETES_CLIENT_KEY_FILE_SYSTEM_PROPERTY,
                     client.getConfiguration().getClientKeyFile());
             System.setProperty(Config.KUBERNETES_TRUST_CERT_SYSTEM_PROPERTY, "true");
-            System.setProperty(Config.KUBERNETES_AUTH_TRYKUBECONFIG_SYSTEM_PROPERTY, "false");
-            System.setProperty(Config.KUBERNETES_HTTP2_DISABLE, "true");
+            System.setProperty(Config.KUBERNETES_AUTH_TRYKUBECONFIG_SYSTEM_PROPERTY, "true");
+            System.setProperty(Config.KUBERNETES_AUTH_TRYSERVICEACCOUNT_SYSTEM_PROPERTY, "true");
             System.setProperty(Config.KUBERNETES_NAMESPACE_SYSTEM_PROPERTY, "default");
         }
     }
@@ -50,6 +59,14 @@ public class IstioServiceIT {
                     .withName("sample-app-with-istio")
                     .get();
             Assertions.assertNotNull(gateway);
+        }
+    }
+
+    @TestConfiguration
+    static class IstioTestConfig {
+        @Bean
+        IstioClient istioClient() {
+            return new DefaultIstioClient().inNamespace("default");
         }
     }
 }
